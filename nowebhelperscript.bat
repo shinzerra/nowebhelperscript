d910f812bf57b8ec3b09c981f3b23a8d2ffbaa54
shin <91288352+shinzerra@users.noreply.github.com>
2024-07-21 12:36:42 +0100

    Update nowebhelperscript.bat

--- nowebhelperscript.bat
+++ nowebhelperscript.bat
@@ -3,9 +3,18 @@
 echo killing steam
 taskkill /f /IM "steam.exe"
 
+echo checking for NoSteamWebHelper.exe
+
+if not exist "C:\Program Files (x86)\Steam\NoSteamWebHelper.exe" (
+    echo NoSteamWebHelper.exe not found.
+    echo make sure it is in the same folder as steam.exe.
+    echo if you don't have it, get it from https://github.com/Aetopia/NoSteamWebHelper
+    timeout /t 10 /nobreak
+    exit /b
+)
+
 echo running steam
-"C:\Program Files (x86)\Steam\NoSteamWebHelper.exe" -sleep
+"C:\Program Files (x86)\Steam\NoSteamWebHelper.exe" -silent
 
-
-echo closing...
+echo Waiting for 5 seconds before closing...
 timeout /t 5 /nobreak
